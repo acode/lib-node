1440ca7c0a904f25cb0abddda02216f84f4b1859
Keith Horwood <keithwhor@gmail.com>
2017-01-24 19:44:55 -0800

    First docs

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# stdlib Package for Node.js
+# StdLib Node.js Bindings
+
+Basic Node bindings for StdLib service accession.
+
+## Usage
+
+```javascript
+const lib = require('lib');
+
+// [1]: Call "stdlib.reflect" function, the latest version, from StdLib
+lib.stdlib.reflect(0, 1, (err, result) => {});
+
+// [2]: Call "stdlib.reflect" function from StdLib, with "dev" environment
+lib.stdlib.reflect['@dev'](0, 1, (err, result) => {});
+
+// [3]: Call "stdlib.reflect" function from StdLib, with "release" environment
+//      This is equivalent to (1)
+lib.stdlib.reflect['@release'](0, 1, (err, result) => {});
+
+// [4]: Call "stdlib.reflect" function from StdLib, with specific version
+//      This is equivalent to (1)
+lib.stdlib.reflect['@0.0.1'](0, 1, (err, result) => {});
+
+// [5]: Call functions within the service (not just the defaultFunction)
+//      This is equivalent to (1) when "main" is the default function
+lib.stdlib.reflect.main(0, 1, (err, result) => {});
+
+// Valid string composition from first parameter only:
+lib['stdlib.reflect'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@dev]'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@release]'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@0.0.1]'](0, 1, (err, result) => {});
+lib['stdlib.reflect.main'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@dev].main'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@release].main'](0, 1, (err, result) => {});
+lib['stdlib.reflect[@0.0.1].main'](0, 1, (err, result) => {});
+```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const path = require('path');
 
 const HOST = 'f.stdlib.com';
 const METHOD = 'POST';
@@ -7,17 +8,73 @@ const PATH = '/';
 
 const lib = (function() {
 
+  function appendVersion(names, str) {
+    if (!str.match(/^@[A-Z0-9\-\.]+$/gi)) {
+      throw new Error(`${names.join('.')} invalid version: ${str}`);
+    }
+    return names.concat(str);
+  }
+
+  function appendPath(names, str) {
+    if (!str.match(/^[A-Z0-9\-]+$/gi)) {
+      if (str.indexOf('@') !== -1) {
+        throw new Error(`${names.join('.')} invalid name: ${str}, please specify versions and environments with [@version]`);
+      }
+      throw new Error(`${names.join('.')} invalid name: ${str}`);
+    }
+    return names.concat(str);
+  }
+
+  function appendLibPath(names, str) {
+
+    names = names ? names.slice() : [];
+    let defaultVersion = '@release';
+
+    if (names.length === 0 && str === '') {
+
+      return names.concat(str);
+
+    } else if (names.length === 0 && str.indexOf('.') !== -1) {
+
+      let versionMatch = str.match(/^[^\.]*?\.[^\.]*?(\[@[^\[\]]*?\])(\.|$)/);
+      let arr;
+
+      if (versionMatch) {
+        version = versionMatch[1];
+        version = version.replace(/^\[?(.*?)\]?$/, '$1');
+        str = str.replace(versionMatch[1], '');
+        arr = str.split('.');
+        arr = arr.slice(0, 2).concat(version, arr.slice(2));
+      } else {
+        arr = str.split('.');
+      }
+
+      while (arr.length && (names = appendLibPath(names, arr.shift())));
+      return names;
+
+    } else if (names.length === 2 && names[0] !== '') {
+
+      return str[0] === '@' ?
+        appendVersion(names, str) :
+        appendPath(appendVersion(names, defaultVersion), str);
+
+    } else {
+
+      return appendPath(names, str);
+
+    }
+
+  }
+
   let LibGen = (names) => {
     names = names || [];
     return new Proxy(
       function __call__() {
 
-        if (!names.length) {
-          return LibGen(arguments[0]);
-        }
-
-        let name = names.slice(0, 2).join('/') + names.slice(2).join('/');
         let args = [].slice.call(arguments);
+
+        let isLocal = !names[0];
+
         let kwargs = {};
         let body;
         let headers;
@@ -48,57 +105,94 @@ const lib = (function() {
           }
         });
 
-        let responded = false;
+        if (isLocal) {
 
-        let req = https.request({
-          host: HOST,
-          method: METHOD,
-          headers: headers,
-          port: PORT,
-          path: `${PATH}${name}`,
-          agent: false
-        }, function (res) {
+          try {
+            pkg = require(path.join(process.cwd(), 'package.json'));
+          } catch (e) {
+            return callback(new Error('Could not run local stdlib function, invalid package.json'));
+          }
 
-          let buffers = [];
-          res.on('data', chunk => buffers.push(chunk));
-          res.on('end', () => {
+          if (!pkg.stdlib) {
+            return callback(new Error('Could not run local stdlib function, no "stdlib" property set in package.json'));
+          }
 
-            var response = Buffer.concat(buffers);
-            var contentType = res.headers['content-type'] || '';
+          names = names.length === 1 && pkg.stdlib.defaultFunction ?
+            names.concat(pkg.stdlib.defaultFunction) :
+            names;
+          let pathname = path.join.apply(null, [process.cwd()].concat('f', names, 'index.js'));
+          let fn;
 
-            if (contentType === 'application/json') {
-              response = response.toString();
-              try {
-                response = JSON.parse(response);
-              } catch(e) {
-                response = null;
+          try {
+            fn = require(pathname);
+          } catch (e) {
+            return callback(e);
+          }
+
+          if (typeof fn !== 'function') {
+            return callback(new Error(`${params.name}: No valid function exported from "${pathname}"`));
+          }
+
+          let fnParams = {args: args, kwargs: kwargs};
+          fnParams.buffer = new Buffer(JSON.stringify(fnParams));
+          fnParams.env = process.env.ENV || 'dev';
+          fnParams.service = '.';
+          fnParams.remoteAddress = '::1';
+          fn(fnParams, callback);
+
+        } else {
+
+          let pathname = names.slice(0, 2).join('/') + names.slice(2).join('/');
+          let responded = false;
+
+          let req = https.request({
+            host: HOST,
+            method: METHOD,
+            headers: headers,
+            port: PORT,
+            path: `${PATH}${pathname}`,
+            agent: false
+          }, function (res) {
+
+            let buffers = [];
+            res.on('data', chunk => buffers.push(chunk));
+            res.on('end', () => {
+
+              var response = Buffer.concat(buffers);
+              var contentType = res.headers['content-type'] || '';
+
+              if (contentType === 'application/json') {
+                response = response.toString();
+                try {
+                  response = JSON.parse(response);
+                } catch(e) {
+                  response = null;
+                }
+              } else if (contentType.match(/^text\/.*$/i)) {
+                response = response.toString();
               }
-            } else if (contentType.match(/^text\/.*$/i)) {
-              response = response.toString();
-            }
 
-            responded = true;
+              responded = true;
 
-            if (((res.statusCode / 100) | 0) !== 2) {
-              return callback(new Error(response));
-            } else {
-              return callback(null, response, res.headers);
-            }
+              if (((res.statusCode / 100) | 0) !== 2) {
+                return callback(new Error(response));
+              } else {
+                return callback(null, response, res.headers);
+              }
+
+            });
 
           });
 
-        });
+          req.on('error', err => responded || callback(err));
+          req.write(body);
+          req.end();
 
-        req.on('error', err => responded || callback(err));
-        req.write(body);
-        req.end();
+        }
 
       },
       {
-        get: (target, name) => {
-          name = names.length === 2 && name[0] !== '@' ? ['@release', name] : [name];
-          return LibGen(names.concat(name));
-        }
+        get: (target, name) => LibGen(appendLibPath(names, name))
       }
     );
   };
